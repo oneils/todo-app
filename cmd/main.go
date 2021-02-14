9a8db04ef9809ce47548bcbe83729678ee0d46e5
alexeybo <lex.nox@gmail.com>
2021-02-14 12:05:43 +0300

    Added viper for config reading

--- cmd/main.go
+++ cmd/main.go
@@ -5,17 +5,28 @@ import (
 	"github.com/oneils/todo-app/pkg/handler"
 	"github.com/oneils/todo-app/pkg/repository"
 	"github.com/oneils/todo-app/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("Error while reading config file %s", err.Error())
+	}
+
 	repos := repository.New()
 	services := service.New(repos)
 	handlers := handler.New(services)
 
 	server := new(todo.Server)
 
-	if err := server.Run("8080", handlers.InitRoutes()); err != nil {
+	if err := server.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("Error while starting http server %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
